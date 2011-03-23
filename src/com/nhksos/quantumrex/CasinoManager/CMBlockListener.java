d55194cbca3eb313050a6ea79852ed7c53a915af
Rafer Wenner <quantumrex250@gmail.com>
2011-03-23 09:14:51 -0500

    First Code update. Added basic plugin management. Very little implementation.

---
+++ src/com/nhksos/quantumrex/CasinoManager/CMBlockListener.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nhksos.quantumrex.CasinoManager;
+
+import org.bukkit.event.block.BlockListener;
+
+/**
+ * @author quantumrex
+ *
+ */
+public class CMBlockListener extends BlockListener {
+
+}
---
+++ src/com/nhksos/quantumrex/CasinoManager/CMPlayerListener.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nhksos.quantumrex.CasinoManager;
+
+import org.bukkit.event.player.PlayerListener;
+
+/**
+ * @author quantumrex
+ *
+ */
+public class CMPlayerListener extends PlayerListener {
+
+}
